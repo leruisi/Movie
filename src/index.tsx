63f1de59635087b68bec1416ef53eca7cc726ee3
leruisi <yuristovskaya7@gmail.com>
2023-11-06 13:27:27 +0200

    цц

---
+++ src/index.tsx
@@ -0,0 +1,13 @@
+import ReactDOM from 'react-dom/client';
+import {RouterProvider} from "react-router-dom";
+
+import {router} from "./router";
+
+
+const root = ReactDOM.createRoot(
+    document.getElementById('root') as HTMLElement
+);
+
+root.render(
+    <RouterProvider router={router}/>
+);
