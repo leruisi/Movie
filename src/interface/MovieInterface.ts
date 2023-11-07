d57f3e5f8ca98db68b54353a74e6dcb35bcd6fe4
leruisi <yuristovskaya7@gmail.com>
2023-11-07 11:15:33 +0200

    error

--- src/interface/MovieInterface.ts
+++ src/interface/MovieInterface.ts
@@ -1,17 +1,16 @@
 export interface IMovie  {
     // page:number;
-    id: number;
-    original_language:string;
-    original_title:string;
-    overview:string;
-    poster_path: string;
-    release_date: string;
-        title: string;
+    id: number,
+    original_language:string,
+    original_title:string,
+    overview:string,
+    poster_path: string,
+    release_date: string,
+        title: string,
         vote_average: number
-
 }
 
-export interface IMovieResonse{
+export interface IMovieResonse {
     page:number,
     results:IMovie[],
     total_pages:number,
